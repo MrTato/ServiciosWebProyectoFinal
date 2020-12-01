c797f5660e5fea1ce38296981c4314365b266091
Hosffman Meza <hemeza@uamv.edu.ni>
2020-12-01 17:43:32 -0600

    Agregación de SP al API

---
+++ APIserviciosV2/Controllers/ListClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIserviciosV2.Models;
+
+namespace APIserviciosV2.Controllers
+{
+    [Authorize]
+    public class ListClientController : ApiController
+    {
+        private BDServicioV2EntitiesSTPR db = new BDServicioV2EntitiesSTPR();
+
+        // GET: api/ListClient
+        public IQueryable<SP_ListClient_Result> GetListClient()
+        {
+            return db.SP_ListClient_Result().AsQueryable();
+        }
+    }
+}
